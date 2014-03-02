ac8df2f83d330f22a3af005fcb76fa92f0316726
Daniel Larsson <daniel.larsson@ctk.se>
2014-03-02 23:38:04 +0100

    Implement NSUserDefaults

--- Meetup/SettingsViewController.m
+++ Meetup/SettingsViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "SettingsViewController.h"
+#import "SettingsHelper.h"
 
 @interface SettingsViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *phoneTextField;
 
 @end
 
@@ -26,18 +29,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    // Load credentials
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [self.nameTextField setText:[defaults objectForKey:@"name"]];
+    [self.phoneTextField setText:[defaults objectForKey:@"phoneNumber"]];
+    NSLog(@"Reading name: %@", [defaults objectForKey:@"name"]);
 }
 
 #pragma mark - Table view data source
@@ -62,55 +59,11 @@
     }
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)viewWillDisappear:(BOOL)animated
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    [[SettingsHelper sharedSettingsHelper] storeName:self.nameTextField.text];
+    [[SettingsHelper sharedSettingsHelper] storePhoneNumber:self.phoneTextField.text];
+    [super viewWillDisappear:animated];
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
- */
 
 @end
